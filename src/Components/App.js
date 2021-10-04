407a2f85f255a0a1dc20bfb7d6942d88bad41f12
Chris Thomason <christhomason@Chriss-MacBook-Pro-2.local>
2021-10-04 15:13:46 +0100

    Added global styles.css and countbutton.css

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import CountButton from "./CountButton";
+import CountButton from "./CountButton/CountButton";
 
 const App = () => {
   return (
     <div>
-      <CountButton incrementBy={1} buttonColor={'blue'} borderRadius = {'10px'} />
-      <CountButton incrementBy={5} buttonColor={'red'} borderRadius = {'3px'}/>
-      <CountButton incrementBy={10} buttonColor={'green'} borerRadius = {'20px'}/>
+      <CountButton incrementBy={1} buttonColor={'blue'}/>
+      <CountButton incrementBy={5} buttonColor={'red'}/>
+      <CountButton incrementBy={10} buttonColor={'green'}/>
     </div>
   );
 };
--- src/Components/CountButton/CountButton.js
+++ src/Components/CountButton/CountButton.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import reactDom from "react-dom";
+import './CountButton.css';
 
 const CountButton = (props) => {
   const [currentCount, setCurrentCount] = useState(0);
@@ -9,14 +10,13 @@ const CountButton = (props) => {
   };
  
   const buttonStyles = {
-    borderRadius: props.borderRadius,
     backgroundColor: props.buttonColor
   }
 
   return (
     <div>
       <button style={buttonStyles}onClick={handleClick}>+{props.incrementBy}</button>
-      <div>{currentCount}</div>
+      <div className ='count-display'> {currentCount}</div>
     </div>
   );
 };
