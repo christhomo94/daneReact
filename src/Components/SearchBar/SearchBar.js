167272ca264328b48ac8ccab32094f8d86dc162e
Chris Thomason <christhomason@Chriss-MacBook-Pro-2.local>
2021-10-05 13:52:55 +0100

    Added product.map to SearchBar component

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,29 +1,42 @@
 import React, { useState } from "react";
 import './SearchBar.css';
 
+const products = [
+    'Tooth Paste',
+    'Tooth Brush',
+    'Mouth Wash',
+    'Dental Floss',
+    'Mouth Guard'
+];
+
 const SearchBar = () => {
 
     const [searchValue, setSearchValue] = useState('');
     const handleInputChange = (event) => {
         setSearchValue(event.target.value)
-    }
+    };
 
     const handleClearClick = () => {
         setSearchValue('')
+    };
 
-    }
-
-    const displayButton = searchValue.length > 0
-
-    console.log(displayButton)
-
+    const displayButton = searchValue.length > 0;
     
     return (
     <div>
         <input type="text" value = {searchValue} onChange = {handleInputChange} />
 
-        {displayButton && <button onClick={handleClearClick}>Clear</button> }
+        {displayButton && <button onClick={handleClearClick}>Clear</button>}
 
+        <ul>
+        {
+            products.map((product) => {
+                return <li key={product}> {product} </li> 
+            })
+        }
+        </ul>
+
+      
     </div>
     )   
     
