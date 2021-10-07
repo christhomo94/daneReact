f03d42c6519b264fe2e407482ac7028e44e03240
Chris Thomason <christhomason@Chriss-MacBook-Pro-2.local>
2021-10-07 11:08:06 +0100

    Added in useEffect & setTimeOut on the product data. Also used a ternary operator to add in a loading message

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CountButton from "./CountButton/CountButton";
 import SearchBar from "./SearchBar/SearchBar";
 
 
 
 const App = () => {
+
+  const [productsState, setProductsState] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setProductsState([
+        'Tooth Paste',
+        'Tooth Brush',
+        'Mouth Wash',
+        'Dental Floss',
+        'Mouth Guard'
+      ])
+    }, 2000)
+  }, [])
+
+  const hasProducts = productsState.length > 0;
+  
   return (
     <div>
-      <SearchBar products={[
-  'Tooth Paste',
-  'Tooth Brush',
-  'Mouth Wash',
-  'Dental Floss',
-  'Mouth Guard'
-]} />
-
-<SearchBar products={[
-  'Bike Rack',
-  'Mountain Bike',
-  'Soccer Ball',
-  'Tennis Racquet',
-  'Rugby Ball'
-]} />
+      {hasProducts ? <SearchBar products={productsState} /> : "Loading..." }
     </div>
   );
 };
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -20,7 +20,7 @@ const SearchBar = (props) => {
     
     return (
     <div>
-        <input type="text" value = {searchValue} onChange = {handleInputChange} />
+        <input type="text" value = {searchValue} onChange = {handleInputChange}/>
         {displayButton && <button onClick={handleClearClick}>Clear</button>}
 
         <ul>
